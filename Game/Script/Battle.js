e1e60818d0a96b7cc5a06430d923e2726f79edeb
pupungeggang6 <pupungeggang6@gmail.com>
2025-05-26 01:30:32 +0900

    Start

---
+++ Game/Script/Battle.js
@@ -0,0 +1,19 @@
+class Battle {
+    constructor() {
+        this.player = new PlayerBattle()
+        this.field = new Field()
+    }
+}
+
+class Field {
+    constructor() {
+        this.cell = []
+        for (let i = 0; i < 9; i++) {
+            let temp = []
+            for (let j = 0; j < 9; j++) {
+                temp.push(new Empty())
+            }
+            this.cell.push(temp)
+        }
+    }
+}
